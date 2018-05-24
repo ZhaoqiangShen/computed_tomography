cb3c04474737bfde8d1f1da012d69418399a0403
Francien Bossema <Francien.Bossema@cwi.nl>
2018-05-24 17:32:32 +0200

    number of circles as input

--- phantoms.py
+++ phantoms.py
@@ -7,18 +7,25 @@ Created on Thu May 24 14:57:09 2018
 """
 import numpy as np
 
-def circle(pic_size):
-    parameters = [pic_size/2, 0.02*pic_size]
+def circle(pic_size, circle_nr):
+    
+    
+    
     shape = np.array([pic_size,pic_size])
     xx = np.arange(0, shape[0]) - shape[0] // 2
     yy = np.arange(0, shape[1]) - shape[1] // 2
     
-    r0 = (parameters[0] - parameters[1])**2
-    r1 = (parameters[0])**2
+    all_circles = np.zeros((pic_size,pic_size))
 
-    vol = ((xx[:, None, None])**2 + (yy[None, :, None])**2)
-    vol = np.array(((vol > r0) & (vol < r1)), dtype = 'float32')  
-    P = np.reshape(vol,(pic_size,pic_size))
+    
+    for i in range(circle_nr):
+        parameters = [pic_size*(circle_nr-i)/(2*circle_nr), 0.02*pic_size]
+        r0 = (parameters[0] - parameters[1])**2
+        r1 = (parameters[0])**2
+        vol = ((xx[:, None, None])**2 + (yy[None, :, None])**2)
+        vol = np.array(((vol > r0) & (vol < r1)), dtype = 'float32') 
+        vol = np.reshape(vol,(pic_size,pic_size))
+        all_circles = all_circles + vol
 
-    return P
+    return all_circles
 
