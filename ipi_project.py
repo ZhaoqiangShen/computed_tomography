025be85ad47a5407dde18a5b603f927041cd394f
Fredrik Gustafsson <fredrikgson@gmail.com>
2018-06-03 16:01:54 +0200

    Get astra projection matrix.

--- ipi_project.py
+++ ipi_project.py
@@ -111,6 +111,47 @@ def load_cached_sinogram(filename):
     return np.load(filename)
 
 
+
+def create_OpTomo(size, proj_size, proj_angles):
+    #size is the image size (square)
+    #proj_size is the proj_size. To avoid problems with backprojection 
+    #take the proj_size to be at least sqrt(2)*size
+    
+    vol_geom = astra.create_vol_geom(size, size)
+    proj_geom = astra.create_proj_geom('parallel', 1.0, proj_size, proj_angles)
+    
+    # For CPU-based algorithms, a "projector" object specifies the projection
+    # model used. In this case, we use the "strip" model.
+    proj_id = astra.create_projector('strip', proj_geom, vol_geom)
+
+
+    W = astra.OpTomo(proj_id)
+
+    return W, proj_id
+
+
+
+def astra_proj_matrix(size, proj_size, proj_angles):
+
+    W, proj_id = create_OpTomo(size, proj_size, proj_angles)
+    
+    # Generate the projection matrix for this projection model.
+    # This creates a matrix W where entry w_{i,j} corresponds to the
+    # contribution of volume element j to detector element i.
+    matrix_id = astra.projector.matrix(proj_id)
+
+    # Get the projection matrix as a Scipy sparse matrix.
+    A = astra.matrix.get(matrix_id)
+    astra.projector.delete(proj_id)
+    astra.matrix.delete(matrix_id)
+
+    return A
+
+
+    
+
+
+
 def dist(img_a, img_b):
 
     norm = np.linalg.norm(img_a - img_b)
