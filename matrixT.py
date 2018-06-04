34c85f0a669b9be4db1b67b3c546c9f0a7ec1a00
Fredrik Gustafsson <fredrikgson@gmail.com>
2018-06-04 10:44:44 +0200

    Create big version of matrix Dx

--- matrixT.py
+++ matrixT.py
@@ -9,6 +9,7 @@ from topolar import topolar
 import numpy as np
 from phantoms import circle
 import pylab
+import scipy.sparse
 
 def create_T(size):
 
@@ -20,26 +21,39 @@ def create_T(size):
         i_vec[i] = 1
         T[:,i] = topolar(np.reshape(i_vec,(size,size)))[0].ravel()
     return T
-    
+
+
+def create_Dx(size):
+
+    # Column version
+    Dx = np.diag([1]*size) -1 * np.diag([1]*(size -1), k=1)
+
+    # Operator that take whole image as one vector (size**2)
+    Dx_big = scipy.sparse.block_diag([Dx for i in range(size)])
+
+    return Dx_big
+
+if __file__ == '__main__':
+
 #%%
     
-size = 64
-T = create_T(size)
-test = circle(size, 4, wall_thickness = 1)
-pylab.imshow(test) 
-pylab.show()
-test_matrix = np.reshape(T@test.ravel(), (size,size))
-pylab.imshow(test_matrix)
-pylab.title('T@test')
-pylab.show()
-test_topolar=topolar(test)[0]
-pylab.imshow(test_topolar) 
-pylab.title('topolar(test)') 
-pylab.show()
-diff = test_matrix-test_topolar
-pylab.imshow(diff)
-pylab.title('difference')
-pylab.show()
-print(diff.max(),diff.min())
+    size = 64
+    T = create_T(size)
+    test = circle(size, 4, wall_thickness = 1)
+    pylab.imshow(test) 
+    pylab.show()
+    test_matrix = np.reshape(T@test.ravel(), (size,size))
+    pylab.imshow(test_matrix)
+    pylab.title('T@test')
+    pylab.show()
+    test_topolar=topolar(test)[0]
+    pylab.imshow(test_topolar) 
+    pylab.title('topolar(test)') 
+    pylab.show()
+    diff = test_matrix-test_topolar
+    pylab.imshow(diff)
+    pylab.title('difference')
+    pylab.show()
+    print(diff.max(),diff.min())
 
 
