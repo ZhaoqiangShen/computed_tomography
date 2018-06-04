Bugfix: correcting the gradient calculation along x.

matrixT.py
@@ -27,11 +27,12 @@ def create_Dx(size):
 
     # Column version
     Dx = np.diag([1]*size) -1 * np.diag([1]*(size -1), k=1)
+    Dx[0, -1] = -1.0
 
     # Operator that take whole image as one vector (size**2)
     Dx_big = scipy.sparse.block_diag([Dx for i in range(size)])
 
-    return Dx_big
+    return Dx_big.T
 
 if __file__ == '__main__':
 